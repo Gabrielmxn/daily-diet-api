2cdb3a7995ea713acc4570720fa70d4f4f004882
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2023-05-01 13:09:04 -0300

    initial

--- db/migrations/20230427143101_create-users.ts
+++ db/migrations/20230427143101_create-users.ts
@@ -4,8 +4,10 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
+    table.text
     table.text('username').notNullable()
     table.text('password').notNullable()
+    table.text('avatar').defaultTo('')
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
 }
